f7e2da9ff4419c5d0a244d1653b5a7129636f631
Patryk <patnow92@wp.pl>
2017-06-06 15:36:31 +0200

    poprawka

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function drawTree(treeHeight) {
-    for ( var i = 1 ; i < treeHeight ; i++ ) {
-    var star = '';
-   for ( var j = 1 ; j <= i ; j++ ) {
-     star += '*';
-   }
-    console.log(star);
-   }
+    for (var i = 1; i < treeHeight; i++) {
+        var star = '';
+        for (var j = 1; j <= i; j++) {
+            star += '*';
+        }
+        console.log(star);
+    }
 }
-  drawTree(5);
+drawTree(5);
